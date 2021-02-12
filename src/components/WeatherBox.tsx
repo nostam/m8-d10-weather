9b80b8a7fdda12050b70ca5307999b8305aee31d
nostam <matsonho@gmail.com>
2021-02-12 18:16:41 +0100

    fix forecast date and time

--- src/components/WeatherBox.tsx
+++ src/components/WeatherBox.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Col } from "react-bootstrap";
 import { WeatherIcon } from "weather-react-icons";
 import { List } from "../types/openWeatherMap";
-// import fromUnixTime from "date-fns/fromUnixTime";
+import dayjs from "dayjs";
+import isoWeek from "dayjs/plugin/isoWeek";
 interface Props {
   data: List;
   //   weatherIcon: () => void;
@@ -10,7 +11,7 @@ interface Props {
 // function WeatherBox({ data }: Props) {
 //   return <div>{data && <div>{weatherIcon(data.weather[0].id)}</div>}</div>;
 // }
-
+dayjs.extend(isoWeek);
 function WeatherBox({ data }: Props) {
   const weatherIcon = (id: number) => {
     return (
@@ -19,17 +20,9 @@ function WeatherBox({ data }: Props) {
         name="owm"
         night
         className=""
-        style={{ fontSize: "2rem" }}
+        style={{ fontSize: "3rem" }}
       />
     );
-  };
-  const now = new Date();
-  const getTime = (time: number): number => {
-    const date = new Date(time * 1000);
-    const hours = date.getHours();
-    const minutes = "0" + date.getMinutes();
-    const seconds = "0" + date.getSeconds();
-    return now.getHours() - hours;
   };
   const shortTemp = (temp: number): string => {
     return Number.parseFloat(temp.toString()).toFixed(1);
@@ -37,11 +30,10 @@ function WeatherBox({ data }: Props) {
   return (
     <div>
       {data && (
-        <Col className="text-center mx-1 px-0 justify-content between">
+        <Col className="text-center mx-1 px-0 justify-content-between">
           <h5>{shortTemp(data.main.temp)}°C</h5>
           {weatherIcon(data.weather[0].id)}
-          {/* {getTime(data.dt)} */}
-          <p>{getTime(data.dt)} hr</p>
+          <p>{dayjs(data.dt_txt).format("ddd hhA")}</p>
         </Col>
       )}
     </div>
